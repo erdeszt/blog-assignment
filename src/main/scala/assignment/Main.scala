be80e9af9153fc4c0549b1cc53baeb11fe8a3d04
erdeszt <erdeszt@gmail.com>
2021-04-06 19:52:56 +0200

    Update todos

--- src/main/scala/assignment/Main.scala
+++ src/main/scala/assignment/Main.scala
@@ -8,14 +8,9 @@ import zio.clock.Clock
 import zio._
 import zio.interop.catz._
 
-/**
-  * TODO:
-  *    - Logging
-  *    - ?Auth?
-  *    - Docs
-  *    - Dockerize
-  *    - Setup CI
-  */
+// TODO: Logging
+// TODO: Dockerize
+// TODO: Auth?
 object Main extends CatsApp {
 
   val layers: URLayer[ZEnv, Has[Migration] with Has[Api]] =
@@ -24,25 +19,19 @@ object Main extends CatsApp {
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
     val server: ZIO[zio.ZEnv with Has[Api] with Has[Migration], Nothing, Unit] = for {
       _ <- Migration.migrate
-      _ <- serve
+      _ <- ZIO.runtime[ZEnv with Has[Api]].flatMap { implicit runtime =>
+        BlazeServerBuilder[RIO[Has[Api] with Clock, *]](runtime.platform.executor.asEC)
+          .bindHttp(8080, "0.0.0.0")
+          .withHttpApp(
+            Router("/" -> Routes.create()).orNotFound,
+          )
+          .resource
+          .use(_ => UIO.never)
+          .orDie
+      }
     } yield ()
 
     server.provideCustomLayer(layers).exitCode
-  }
-
-  // TODO: Swagger docs
-  // TODO: Inline?
-  def serve: URIO[ZEnv with Has[Api], Unit] = {
-    ZIO.runtime[ZEnv with Has[Api]].flatMap { implicit runtime =>
-      BlazeServerBuilder[RIO[Has[Api] with Clock, *]](runtime.platform.executor.asEC)
-        .bindHttp(8080, "0.0.0.0")
-        .withHttpApp(
-          Router("/" -> Routes.create()).orNotFound,
-        )
-        .resource
-        .use(_ => UIO.never)
-        .orDie
-    }
   }
 
 }
