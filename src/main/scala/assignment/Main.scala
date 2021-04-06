9e1db40d0e10845e4703f14ffbd9bc562bed086d
erdeszt <erdeszt@gmail.com>
2021-04-06 20:03:24 +0200

    Fix warnigns

--- src/main/scala/assignment/Main.scala
+++ src/main/scala/assignment/Main.scala
@@ -8,6 +8,8 @@ import zio.clock.Clock
 import zio._
 import zio.interop.catz._
 
+import scala.annotation.nowarn
+
 // TODO: Logging
 // TODO: Auth?
 // TODO: Dockerize
@@ -16,24 +18,23 @@ object Main extends CatsApp {
   val layers: URLayer[ZEnv, Has[Migration] with Has[Api]] =
     (DatabaseConfig.layer >>> Migration.layer) ++ Layers.api
 
+  @nowarn // Dead code warnings for using the resource forever
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
     val server: ZIO[zio.ZEnv with Has[Api] with Has[Migration], Nothing, Unit] = for {
       _ <- Migration.migrate
-      _ <- serve
+      _ <- ZIO.runtime[ZEnv with Has[Api]].flatMap { implicit runtime =>
+        BlazeServerBuilder[RIO[Has[Api] with Clock, *]](runtime.platform.executor.asEC)
+          .bindHttp(8080, "0.0.0.0")
+          .withHttpApp(
+            Router("/" -> Routes.create()).orNotFound,
+          )
+          .resource
+          .use(_ => UIO.never)
+          .orDie
+      }
     } yield ()
 
     server.provideCustomLayer(layers).exitCode
-  }
-
-  val serve = ZIO.runtime[ZEnv with Has[Api]].flatMap { implicit runtime =>
-    BlazeServerBuilder[RIO[Has[Api] with Clock, *]](runtime.platform.executor.asEC)
-      .bindHttp(8080, "0.0.0.0")
-      .withHttpApp(
-        Router("/" -> Routes.create()).orNotFound,
-      )
-      .resource
-      .use(_ => UIO.never)
-      .orDie
   }
 
 }
