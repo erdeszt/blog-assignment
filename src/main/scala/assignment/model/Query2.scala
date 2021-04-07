da6aacecb1201ef2164eb3c03b730e22b2bf4646
erdeszt <erdeszt@gmail.com>
2021-04-07 13:33:35 +0200

    Better representation

--- src/main/scala/assignment/model/Query2.scala
+++ src/main/scala/assignment/model/Query2.scala
@@ -3,70 +3,141 @@ package assignment.model
 import doobie._
 import doobie.syntax.string._
 
+// TODO: Better error handling
+// TODO: Type checking
+// TODO: `HAVING` for `post.view_count`
+// TODO: Explore idea: introduce an intermediate representation ("minisql") and compile the query
+//       to it before rendering to eliminate the null comparison issue
 object Query2 {
 
+  /*
+
+   Default JSON representation
+
+  { "Comparison": { "op": "eq", "field": "blog.id", "value": "blog1" }}
+
+  { "And": { "left": { "Comparison": { "op": "eq", "field": "blog.id", "value": "blog1" }} },
+           { "right": { "Comparison": { "op": "gt", "field": "post.view_count", "value": 0 }} } }
+
+   */
+
   sealed trait Condition
-  final case class Comparison(op: Comparison.Op, field: FieldSelector, value: Value) extends Condition
-  final case class And(left:      Condition, right:     Condition) extends Condition
-  final case class Or(left:       Condition, right:     Condition) extends Condition
+  final case class BinOp(op: BinOpType, field: FieldSelector, value: Value) extends Condition
+  final case class UnOp(op:  UnOpType, field:  FieldSelector) extends Condition
+  final case class And(left: Condition, right: Condition) extends Condition
+  final case class Or(left:  Condition, right: Condition) extends Condition
 
-  object Comparison {
-    sealed trait Op
-    final case class Eq() extends Op
-    final case class NotEq() extends Op
-    final case class Lt() extends Op
-    final case class Gt() extends Op
-    final case class Lte() extends Op
-    final case class Gte() extends Op
-    final case class Like() extends Op
+  sealed trait BinOpType
+  final case class Eq() extends BinOpType
+  final case class NotEq() extends BinOpType
+  final case class Lt() extends BinOpType
+  final case class Gt() extends BinOpType
+  final case class Lte() extends BinOpType
+  final case class Gte() extends BinOpType
+  final case class Like() extends BinOpType
+
+  sealed trait UnOpType
+  final case class IsNull() extends UnOpType
+  final case class IsNotNull() extends UnOpType
+
+  sealed abstract class FieldSelector(val nullable: Boolean) {
+    type ValueType <: Value
   }
-
-  final case class FieldSelector(model: String, field: String)
+  object FieldSelector {
+    object Blog {
+      final case class Id() extends FieldSelector(nullable   = false) { type ValueType = Value.Text }
+      final case class Name() extends FieldSelector(nullable = false) { type ValueType = Value.Text }
+      final case class Slug() extends FieldSelector(nullable = false) { type ValueType = Value.Text }
+    }
+    object Post {
+      final case class Id() extends FieldSelector(nullable        = false) { type ValueType = Value.Text }
+      final case class Title() extends FieldSelector(nullable     = true) { type ValueType  = Value.Text }
+      final case class Content() extends FieldSelector(nullable   = false) { type ValueType = Value.Text }
+      final case class ViewCount() extends FieldSelector(nullable = false) { type ValueType = Value.Number }
+    }
+  }
 
   sealed trait Value
   object Value {
-    final case class Null() extends Value
     final case class Number(value: Long) extends Value
     final case class Text(value:   String) extends Value
   }
 
-  object Compiler {
+  object TypeChecker {
 
-    sealed abstract class CompileError(message: String) extends Exception(message)
-    final case class InvalidNullComparison(op:  Comparison.Op)
-        extends CompileError(s"Invalid Null comparison with operator: ${op}")
+    sealed abstract class TypeCheckerError(message:                    String) extends Exception(message)
+    final case class NullComparisonWithNonNullableField(fieldSelector: FieldSelector)
+        extends TypeCheckerError(s"Comparing non nullable field ${fieldSelector} with NULL")
+
+    def check(condition: Condition): Unit = {
+      condition match {
+        case UnOp(IsNull(), selector) if !selector.nullable =>
+          throw NullComparisonWithNonNullableField(selector)
+        case UnOp(IsNotNull(), selector) if !selector.nullable =>
+          throw NullComparisonWithNonNullableField(selector)
+        case And(left, right) => {
+          check(left)
+          check(right)
+        }
+        case Or(left, right) => {
+          check(left)
+          check(right)
+        }
+      }
+    }
+
+  }
+
+  object Compiler {
 
     def compile(condition: Condition): Fragment = {
       condition match {
-        case Comparison(op, field, value) => compileOp(op, field, value)
-        case And(left, right)             => Fragments.and(compile(left), compile(right))
-        case Or(left, right)              => Fragments.or(compile(left), compile(right))
+        case BinOp(op, field, value) => compileBinOp(op, field, value)
+        case UnOp(op, field)         => compileUnOp(op, field)
+        case And(left, right)        => Fragments.and(compile(left), compile(right))
+        case Or(left, right)         => Fragments.or(compile(left), compile(right))
       }
     }
 
-    // TODO: Better error handling
-    def compileOp(op: Comparison.Op, field: FieldSelector, value: Value): Fragment = {
-      (op, value) match {
-        case (Comparison.Eq(), Value.Null())    => renderField(field) ++ Fragment.const("IS") ++ renderValue(value)
-        case (Comparison.NotEq(), Value.Null()) => renderField(field) ++ Fragment.const("IS NOT") ++ renderValue(value)
-        case (_, Value.Null())                  => throw InvalidNullComparison(op)
-        case (Comparison.Eq(), _)               => renderField(field) ++ Fragment.const("=") ++ renderValue(value)
-        case (Comparison.NotEq(), _)            => renderField(field) ++ Fragment.const("!=") ++ renderValue(value)
-        case (Comparison.Lt(), _)               => renderField(field) ++ Fragment.const("<") ++ renderValue(value)
-        case (Comparison.Gt(), _)               => renderField(field) ++ Fragment.const(">") ++ renderValue(value)
-        case (Comparison.Lte(), _)              => renderField(field) ++ Fragment.const("<=") ++ renderValue(value)
-        case (Comparison.Gte(), _)              => renderField(field) ++ Fragment.const(">=") ++ renderValue(value)
-        case (Comparison.Like(), _)             => renderField(field) ++ Fragment.const("LIKE") ++ renderValue(value)
+    def compileBinOp(op: BinOpType, field: FieldSelector, value: Value): Fragment = {
+      val opFragment = op match {
+        case Eq()    => Fragment.const("=")
+        case NotEq() => Fragment.const("!=")
+        case Lt()    => Fragment.const("<")
+        case Gt()    => Fragment.const(">")
+        case Lte()   => Fragment.const("<=")
+        case Gte()   => Fragment.const(">=")
+        case Like()  => Fragment.const("LIKE")
       }
+
+      renderField(field) ++ opFragment ++ renderValue(value)
+    }
+
+    def compileUnOp(op: UnOpType, field: FieldSelector): Fragment = {
+      val opFragment = op match {
+        case IsNull()    => Fragment.const("IS NULL")
+        case IsNotNull() => Fragment.const("IS NOT NULL")
+      }
+
+      renderField(field) ++ opFragment
     }
 
     def renderField(field: FieldSelector): Fragment = {
-      Fragment.const(s"${field.model}.${field.field}")
+      val (model, fieldName) = field match {
+        case FieldSelector.Blog.Id()        => ("blog", "id")
+        case FieldSelector.Blog.Name()      => ("blog", "name")
+        case FieldSelector.Blog.Slug()      => ("blog", "slug")
+        case FieldSelector.Post.Id()        => ("post", "id")
+        case FieldSelector.Post.Title()     => ("post", "title")
+        case FieldSelector.Post.Content()   => ("post", "content")
+        case FieldSelector.Post.ViewCount() => ("post", "view_count")
+
+      }
+      Fragment.const(s"${model}.${fieldName}")
     }
 
     def renderValue(value: Value): Fragment = {
       value match {
-        case Value.Null()         => Fragment.const("NULL")
         case Value.Number(number) => fr"${number}"
         case Value.Text(text)     => fr"${text}"
       }
--- src/test/scala/assignment/QuerySpec.scala
+++ src/test/scala/assignment/QuerySpec.scala
@@ -1,6 +1,7 @@
 package assignment
 
 import assignment.model.Query2.Compiler._
+import assignment.model.Query2.TypeChecker.NullComparisonWithNonNullableField
 import assignment.model.Query2._
 import doobie.syntax.string._
 import zio._
@@ -12,111 +13,114 @@ object QuerySpecSbtRunner extends QuerySpec
 class QuerySpec extends JUnitRunnableSpec {
 
   val blogId            = "blog1"
-  val blogIdSelector    = FieldSelector("blog", "id")
-  val viewCountSelector = FieldSelector("post", "view_count")
-  val postTitleSelector = FieldSelector("post", "title")
+  val blogIdSelector    = FieldSelector.Blog.Id()
+  val viewCountSelector = FieldSelector.Post.ViewCount()
+  val postTitleSelector = FieldSelector.Post.Title()
 
   // TODO: Type check fields
   override def spec = suite("Query")(
-    test("compile conditions") {
-      val queriesAndFragments = List(
-        Comparison(Comparison.Eq(), blogIdSelector, Value.Text(blogId))   -> fr"blog.id = ${blogId}",
-        Comparison(Comparison.Eq(), blogIdSelector, Value.Null())         -> fr"blog.id IS NULL",
-        Comparison(Comparison.NotEq(), blogIdSelector, Value.Null())      -> fr"blog.id IS NOT NULL",
-        Comparison(Comparison.Lt(), viewCountSelector, Value.Number(12))  -> fr"post.view_count < ${12}",
-        Comparison(Comparison.Gt(), viewCountSelector, Value.Number(12))  -> fr"post.view_count > ${12}",
-        Comparison(Comparison.Lte(), viewCountSelector, Value.Number(12)) -> fr"post.view_count <= ${12}",
-        Comparison(Comparison.Gte(), viewCountSelector, Value.Number(12)) -> fr"post.view_count >= ${12}",
-      )
+    suite("Compiler")(
+      test("compile conditions") {
+        val queriesAndFragments = List(
+          BinOp(Eq(), blogIdSelector, Value.Text(blogId))   -> fr"blog.id = ${blogId}",
+          UnOp(IsNull(), blogIdSelector)                    -> fr"blog.id IS NULL",
+          UnOp(IsNotNull(), blogIdSelector)                 -> fr"blog.id IS NOT NULL",
+          BinOp(Lt(), viewCountSelector, Value.Number(12))  -> fr"post.view_count < ${12}",
+          BinOp(Gt(), viewCountSelector, Value.Number(12))  -> fr"post.view_count > ${12}",
+          BinOp(Lte(), viewCountSelector, Value.Number(12)) -> fr"post.view_count <= ${12}",
+          BinOp(Gte(), viewCountSelector, Value.Number(12)) -> fr"post.view_count >= ${12}",
+        )
 
-      queriesAndFragments.foldLeft(assert(true)(equalTo(true))) {
-        case (assertions, (query, expected)) =>
-          val renderedQuery = compile(query)
+        queriesAndFragments.foldLeft(assert(true)(equalTo(true))) {
+          case (assertions, (query, expected)) =>
+            val renderedQuery = compile(query)
 
-          assertions && assert(renderedQuery.toString)(equalTo(expected.toString))
-      }
-    },
-    testM("fail for invalid Null comparison") {
-      val invalidOp = Comparison.Lt()
-      val query     = Comparison(invalidOp, blogIdSelector, Value.Null())
-      for {
-        error <- Task(compile(query)).either
-      } yield assert(error)(isLeft(equalTo(InvalidNullComparison(invalidOp))))
-    },
-    test("||") {
-      val query = Or(
-        Comparison(Comparison.Eq(), blogIdSelector, Value.Text(blogId)),
-        Comparison(Comparison.Gt(), viewCountSelector, Value.Number(0)),
-      )
-      val renderedQuery = compile(query)
+            assertions && assert(renderedQuery.toString)(equalTo(expected.toString))
+        }
+      },
+      test("||") {
+        val query = Or(
+          BinOp(Eq(), blogIdSelector, Value.Text(blogId)),
+          BinOp(Gt(), viewCountSelector, Value.Number(0)),
+        )
+        val renderedQuery = compile(query)
 
-      assert(renderedQuery.toString)(equalTo(fr"(blog.id = ${blogId} ) OR (post.view_count > ${0} )".toString))
-    },
-    test("|| 3") {
-      val query = Or(
-        Comparison(Comparison.Eq(), blogIdSelector, Value.Text(blogId)),
-        Or(
-          Comparison(Comparison.Gt(), viewCountSelector, Value.Number(0)),
-          Comparison(Comparison.NotEq(), postTitleSelector, Value.Null()),
-        ),
-      )
-      val renderedQuery = compile(query)
+        assert(renderedQuery.toString)(equalTo(fr"(blog.id = ${blogId} ) OR (post.view_count > ${0} )".toString))
+      },
+      test("|| 3") {
+        val query = Or(
+          BinOp(Eq(), blogIdSelector, Value.Text(blogId)),
+          Or(
+            BinOp(Gt(), viewCountSelector, Value.Number(0)),
+            UnOp(IsNotNull(), postTitleSelector),
+          ),
+        )
+        val renderedQuery = compile(query)
 
-      assert(renderedQuery.toString)(
-        equalTo(fr"(blog.id = ${blogId} ) OR ((post.view_count > ${0} ) OR (post.title IS NOT NULL ) )".toString),
-      )
-    },
-    test("&&") {
-      val query = And(
-        Comparison(Comparison.Eq(), blogIdSelector, Value.Text(blogId)),
-        Comparison(Comparison.Gt(), viewCountSelector, Value.Number(0)),
-      )
-      val renderedQuery = compile(query)
+        assert(renderedQuery.toString)(
+          equalTo(fr"(blog.id = ${blogId} ) OR ((post.view_count > ${0} ) OR (post.title IS NOT NULL ) )".toString),
+        )
+      },
+      test("&&") {
+        val query = And(
+          BinOp(Eq(), blogIdSelector, Value.Text(blogId)),
+          BinOp(Gt(), viewCountSelector, Value.Number(0)),
+        )
+        val renderedQuery = compile(query)
 
-      assert(renderedQuery.toString)(equalTo(fr"(blog.id = ${blogId} ) AND (post.view_count > ${0} )".toString))
-    },
-    test("&& 3") {
-      val query = And(
-        Comparison(Comparison.Eq(), blogIdSelector, Value.Text(blogId)),
-        And(
-          Comparison(Comparison.Gt(), viewCountSelector, Value.Number(0)),
-          Comparison(Comparison.NotEq(), postTitleSelector, Value.Null()),
-        ),
-      )
-      val renderedQuery = compile(query)
+        assert(renderedQuery.toString)(equalTo(fr"(blog.id = ${blogId} ) AND (post.view_count > ${0} )".toString))
+      },
+      test("&& 3") {
+        val query = And(
+          BinOp(Eq(), blogIdSelector, Value.Text(blogId)),
+          And(
+            BinOp(Gt(), viewCountSelector, Value.Number(0)),
+            UnOp(IsNotNull(), postTitleSelector),
+          ),
+        )
+        val renderedQuery = compile(query)
 
-      assert(renderedQuery.toString)(
-        equalTo(fr"(blog.id = ${blogId} ) AND ((post.view_count > ${0} ) AND (post.title IS NOT NULL ) )".toString),
-      )
-    },
-    test("|| &&") {
-      val query = Or(
-        Comparison(Comparison.Eq(), blogIdSelector, Value.Text(blogId)),
-        And(
-          Comparison(Comparison.Gt(), viewCountSelector, Value.Number(0)),
-          Comparison(Comparison.NotEq(), postTitleSelector, Value.Null()),
-        ),
-      )
-      val renderedQuery = compile(query)
+        assert(renderedQuery.toString)(
+          equalTo(fr"(blog.id = ${blogId} ) AND ((post.view_count > ${0} ) AND (post.title IS NOT NULL ) )".toString),
+        )
+      },
+      test("|| &&") {
+        val query = Or(
+          BinOp(Eq(), blogIdSelector, Value.Text(blogId)),
+          And(
+            BinOp(Gt(), viewCountSelector, Value.Number(0)),
+            UnOp(IsNotNull(), postTitleSelector),
+          ),
+        )
+        val renderedQuery = compile(query)
 
-      assert(renderedQuery.toString)(
-        equalTo(fr"(blog.id = ${blogId} ) OR ((post.view_count > ${0} ) AND (post.title IS NOT NULL ) )".toString),
-      )
-    },
-    test("&& || ") {
-      val query = And(
-        Comparison(Comparison.Eq(), blogIdSelector, Value.Text(blogId)),
-        Or(
-          Comparison(Comparison.Gt(), viewCountSelector, Value.Number(0)),
-          Comparison(Comparison.NotEq(), postTitleSelector, Value.Null()),
-        ),
-      )
-      val renderedQuery = compile(query)
+        assert(renderedQuery.toString)(
+          equalTo(fr"(blog.id = ${blogId} ) OR ((post.view_count > ${0} ) AND (post.title IS NOT NULL ) )".toString),
+        )
+      },
+      test("&& || ") {
+        val query = And(
+          BinOp(Eq(), blogIdSelector, Value.Text(blogId)),
+          Or(
+            BinOp(Gt(), viewCountSelector, Value.Number(0)),
+            UnOp(IsNotNull(), postTitleSelector),
+          ),
+        )
+        val renderedQuery = compile(query)
 
-      assert(renderedQuery.toString)(
-        equalTo(fr"(blog.id = ${blogId} ) AND ((post.view_count > ${0} ) OR (post.title IS NOT NULL ) )".toString),
-      )
-    },
+        assert(renderedQuery.toString)(
+          equalTo(fr"(blog.id = ${blogId} ) AND ((post.view_count > ${0} ) OR (post.title IS NOT NULL ) )".toString),
+        )
+      },
+    ),
+    suite("Type checker")(
+      testM("should not allow null comparison with non nullable fields") {
+        val query = UnOp(IsNull(), blogIdSelector)
+        for {
+          error <- Task(TypeChecker.check(query)).either
+        } yield assert(error)(isLeft(equalTo(NullComparisonWithNonNullableField(blogIdSelector))))
+      },
+    ),
   )
 
 }
